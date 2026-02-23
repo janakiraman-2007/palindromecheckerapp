a64dc852a805ec4c4cd6a9c23eb1173291bf9c7c
janakiraman-2007 <janakiramana2007@gmail.com>
2026-02-23 14:36:55 +0530

    UC1: Application Entry & Welcome Message

---
+++ palindromechecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,5 @@
+public class PalindromeCheckerApp {
+    static void main() {
+        System.out.println(" welcome to the palindromechecker management system\n version:1.0\n initiated successfully");
+    }
+}
